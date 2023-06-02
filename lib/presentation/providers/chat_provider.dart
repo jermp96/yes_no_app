06dd34f9a272a6944081b352899484d1b413176c
jermp96 <jjeremyperez96@gmail.com>
2023-06-02 17:50:51 -0600

    *show provider messages

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -4,7 +4,7 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
 
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hi', fromWho: FromWho.me),
     Message(text: 'do you wanna go to...?', fromWho: FromWho.me),
   ];
