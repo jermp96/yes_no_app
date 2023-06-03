0f1c1be099cfcaea1e978c8bc0c8bf7cff8461ed
jermp96 <jjeremyperez96@gmail.com>
2023-06-03 12:07:21 -0600

    *mappers

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,5 +1,6 @@
 
 import 'package:dio/dio.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 import '../../domain/entities/message.dart';
 
@@ -9,6 +10,12 @@ class GetYesNoAnswer {
 
     final response = await _dio.get('https://yesno.wtf/api');
 
-    throw UnimplementedError();
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    return Message(
+      text: yesNoModel.answer,
+      fromWho: FromWho.hers,
+      imageUrl: yesNoModel.image
+    );
   }
 }
